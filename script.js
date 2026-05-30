5b251b74662c9512f6de9e302d7ed266c7034496
AzureDreams <AzureDreams@users.noreply.github.com>
2026-05-30 16:18:02 -0500

    finishd startWash function

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const startWash = document.getElementById("start-wash");
+const startWashing = document.getElementById("start-wash");
 const endWash = document.getElementById("end-wash");
+const state = document.getElementById("state");
+const washerImg = document.getElementById("washer");
 
 function startWash (){
-  
+  startWashing.addEventListener('click', () => {
+    state.textContent = 'Washing has started!';
+    washerImg.src ="/images/washing-washer.png"; 
+    console.log("Wash Starting");
+  });
 }
+
+startWash();
+
