8dfefa24451de92ed1fe5b31fd885f1dff88325f
AzureDreams <AzureDreams@users.noreply.github.com>
2026-06-13 01:21:49 -0500

    removed unnecessary function wrap

--- script.js
+++ script.js
@@ -3,26 +3,23 @@ const endWashing = document.getElementById("end-wash");
 const state = document.getElementById("state");
 const washerImg = document.getElementById("washer");
 
-function startWash (){
   startWashing.addEventListener('click', () => {
     state.textContent = 'Washing has started!';
     washerImg.src ="/images/washing-washer.png"; 
-    console.log("Wash Starting");
+    
+
   });
-}
 
-function endWash (){
   endWashing.addEventListener('click', ()=> {
-state.textContent = 'Washing has ended';
-washerImg.src = "/images/deafult-washer.png";
-console.log("Wash is ending ");
+  state.textContent = 'Washing has ended';
+  washerImg.src = "/images/deafult-washer.png";
+  console.log("Wash is ending ");
+  setTimeout(() => {
+    state.textContent = 'Waiting for a load to Wash';
+  },3000);
   })
-}
 
- setTimeout(() => {
-  state.textContent = "Waiting for a load to Wash!";
- }, 3000);
-startWash();
-endWash();
+
+
 
 
